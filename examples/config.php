77fa429d7f908fa87837d15d28a868af9b6390a9
Johnny Mast <mastjohnny@gmail.com>
2015-12-29 15:11:50 +0100

    Added a few use cases in examples and a general config file

--- examples/config.php
+++ examples/config.php
@@ -11,7 +11,7 @@ session_start();
 |
 | It wil provide you with the information that you will need in this file.
 */
-$config['twitch_client_id']     = 'p676m8mx4e2zh7es4c2wxiul07w43qw';
-$config['twitch_client_secret'] = '9xhkpcgiwv5tg2yypl4ut16gtdke59o';
+$config['twitch_client_id']     = '';
+$config['twitch_client_secret'] = '';
 $config['twitch_redirect_uri']  = 'http://localhost/examples/authorize-returning-user.php';
 $config['twitch_force_login']   = true;
