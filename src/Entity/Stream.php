60f2a3617d2fa988c67a9fa4a1dd0884cb0513f5
Johnny Mast <mastjohnny@gmail.com>
2016-01-03 13:59:21 +0100

    Added entity abstract

--- src/Entity/Stream.php
+++ src/Entity/Stream.php
@@ -1,7 +1,59 @@
 <?php
 namespace Redbox\Twitch\Entity;
 
+/**
+ * Class Stream
+ * @package Redbox\Twitch\Entity
+ */
 class Stream extends EntityAbstract{
-    protected $abc;
 
+    /**
+     * @var string
+     */
+    protected $video_height;
+
+    /**
+     * @var string
+     */
+    protected $average_fps;
+
+    /**
+     * @var string
+     */
+    protected $is_playlist;
+
+    /**
+     * @var string
+     */
+    protected $created_at;
+
+    /**
+     * @var \stdClass
+     */
+    protected $channel;
+
+    /**
+     * @var \stdClass
+     */
+    protected $preview;
+
+    /**
+     * @var string
+     */
+    protected $viewers;
+
+    /**
+     * @var string
+     */
+    protected $delay;
+
+    /**
+     * @var string
+     */
+    protected $game;
+
+    /**
+     * @var int
+     */
+    protected $id;
 }
